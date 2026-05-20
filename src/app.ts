ca779ac5cfbd1da80109d6c8401be03c06dbf9f3
PlayRunOffc <playruns3@gmail.com>
2026-05-19 21:31:33 -0300

    user routes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import cors from "cors";
-import routes from "./routes/routes";
+import routes from "./routes/user.route";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", routes);
+app.use("/api/user", routes);
 
 export { app };
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -2,4 +2,6 @@ import { Router } from "express";
 
 const routes = Router();
 
+
+
 export default routes;
